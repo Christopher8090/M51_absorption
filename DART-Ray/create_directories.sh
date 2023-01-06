1fa6c965b6666abc21ea43d86bec1e4793aa3a35
Christopher Inman <Chrisinman8090@gmail.com>
2023-01-06 09:07:15 +0000

    update and add create_directories.sh in DART-Ray/ and emission_NUrad/

--- DART-Ray/create_directories.sh
+++ DART-Ray/create_directories.sh
@@ -1,11 +1,22 @@
 #!/bin/tcsh
 
+if ( ! -d "./MAPS/") then
+	mkdir ./MAPS/
+	echo "create directory: ./MAPS/"
+endif
+
 if ( ! -d "./MAPS/M51a/") then
-	mkdir ./MAPS/M51a/
-	echo "create directory: ./MAPS/M51a/"
+        mkdir ./MAPS/M51a/
+        echo "create directory: ./MAPS/M51a/"
+endif
+
+if ( ! -d "./RUNS/") then
+        mkdir ./RUNS/
+        echo "create directory: ./RUNS/"
 endif
 
 if ( ! -d "./RUNS/GALAXY_M51a/") then
         mkdir ./RUNS/GALAXY_M51a/
         echo "create directory: ./RUNS/GALAXY_M51a/"
 endif
+
